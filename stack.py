3840733081efc695d64018a4928b897febf9fee1
trivialcoding6 <167159243+trivialcoding6@users.noreply.github.com>
2025-02-07 22:08:44 +0900

    Feature/task d (#9)
    * 커 밋 1
    
    * 커 밋 2
    
    * 커 밋 3
    
    * 커 밋 4
    
    * 커 밋 5

--- stack.py
+++ stack.py
@@ -7,13 +7,14 @@ class Stack:
         if not self.empty():
             return self.stack.pop()
         return -1
-    def peek(self):
+    
+    def top(self):
         if not self.empty():
             return self.stack[-1]
-        return -1
+        return False
     
     def empty(self):
-        return True if len(self.stack) == 0 else False
+        return 1 if len(self.stack) == 0 else 0
     
     def size(self):
         return len(self.stack)
@@ -25,11 +26,14 @@ stack = Stack()
 for _ in range(N):
     command = input().split()
     if command[0] == 'push':
+
         stack.push(command[1])
     elif command[0] == 'pop':
         print(stack.pop())
+
     elif command[0] == 'top':
         print(stack.top())
+
     elif command[0] == 'size':
         print(stack.size())
     elif command[0] == 'empty':
