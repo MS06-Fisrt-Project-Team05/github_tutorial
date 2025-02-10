1fa0c9f4114b1385a489510677398db93755f878
trivialcoding6 <167159243+trivialcoding6@users.noreply.github.com>
2025-02-10 19:36:42 +0900

    Feature/squash test (#12)
    * 커밋 1
    
    * 커밋 2

--- stack.py
+++ stack.py
@@ -1,10 +1,11 @@
 class Stack:
     def __init__(self):
         self.stack = []
+        
     def push(self, item):
         self.stack.append(item)
+
     def peek(self):
-        
         if not self.empty():
             return self.stack.pop()
         return False
